12447c12c45a24e373cd9fed0b93c107e129954d
David Frese <david.frese@active-group.de>
2021-04-19 15:07:20 +0200

    Fix 'onDoubleClick', and add not about more problematic event names.

--- src/reacl_c/impl/dom0.cljs
+++ src/reacl_c/impl/dom0.cljs
@@ -1,5 +1,6 @@
 (ns ^:no-doc reacl-c.impl.dom0
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            #_["react-dom" :as react-dom]))
 
 (defn capture-event? [k]
   (let [s (name k)]
@@ -7,12 +8,20 @@
          (or (str/ends-with? s "Capture")
              (str/ends-with? s "capture")))))
 
+(defn- react-handler-name [ev-type]
+  ;; Usually "click" -> "onClick", but unfortunately, there are some exceptions to it:
+  ;; TODO: hook up with https://github.com/facebook/react/blob/master/packages/react-dom/src/events/DOMEventProperties.js for all of them
+  ;; not exported though :-( (js/console.log react-dom/events.-DOMEventProperties.-topLevelEventsToReactNames)
+  (case ev-type
+    "dblclick" "ondoubleclick"
+    (str "on" ev-type)))
+
 (defn find-event-handler [events capture? ev]
   ;; OPT: could be done a little faster - try a lookup, or prepare a different map. But it can be 'onchange' and 'onChange'.
   (when-not (.-type ev)
     (js/console.error ev)
     (throw (ex-info "Expected a JavaScript event object, with a property 'type'." {:value ev})))
-  (let [en (cond-> (str "on" (str/lower-case (.-type ev)))
+  (let [en (cond-> (react-handler-name (.-type ev))
              capture? (str "capture"))]
     (some (fn [[n f]]
             (and (= en (str/lower-case (name n)))
