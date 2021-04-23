a52e4c20ee0103f09196e35b627a9265141d5095
David Frese <david.frese@active-group.de>
2021-04-23 16:49:44 +0200

    Cleanup todo example; use with-bind.

--- examples/todo/core.cljs
+++ examples/todo/core.cljs
@@ -5,51 +5,6 @@
               [reacl-c.main :as main]
               [reacl-c.dom :as dom]))
 
-(defrecord Call [id f args])
-
-(def get-unique-id
-  (c/handle-effect-result (fn [_ uuid]
-                            uuid)
-                          (c/effect random-uuid)))
-
-(let [binder (fn [id h & [translate-args]] ;; translate-args -> fn[inner-args]
-               (when (some? h)
-                 (fn [inner-state & args]
-                   (c/return :action (Call. id h (if (some? translate-args) ;; TODO: translation based on inner-state? desireable? (can also use bind in a local handler then if that's equivalent?)
-                                                   [(apply translate-args args)] ;; TODO: allow multiple values (with special return type); although handler should usually only have one arg.
-                                                   args))))))
-      handler (fn [id state a]
-                (if (and (instance? Call a)
-                         (= id (:id a)))
-                  (apply (:f a) state (:args a))
-                  (c/return :action a)))]
-  (c/defn-item with-bind [f] ;; ... and as a macro? with-bind-as
-    (c/with-state-as [_ id :local nil]
-      (c/fragment (c/focus lens/second get-unique-id)
-                  (when id
-                    (c/focus lens/first
-                             (-> (f (f/partial binder id))
-                                 (c/handle-action (f/partial handler id)))))))))
-
-(defn- lift-return [r]
-  (if (c/returned? r) r (c/return :state r)))
-
-(defn- right-state [state ret]
-  (if (= c/keep-state (c/returned-state ret))
-    state
-    (c/returned-state ret)))
-
-(defn merge-handlers [& handlers]
-  (when-let [hs (not-empty (remove nil? handlers))]
-    (fn [state & args]
-      (reduce (fn [ret h]
-                (let [ret2 (lift-return (apply h (right-state state ret) args))]
-                  (c/merge-returned ret ret2)))
-              (c/return)
-              hs))))
-
-;; -----
-
 (defn checked-state [_ e]
   (.. e -target -checked))
 
@@ -57,7 +12,7 @@
   (c/with-state-as checked
     (dom/input {:type "checkbox"
                 :value checked
-                :onchange checked-state})))
+                :onChange checked-state})))
 
 (defn value-state [_ e]
   (.. e -target -value))
@@ -66,23 +21,23 @@
   (c/with-state-as value
     (dom/input {:type "text"
                 :value value
-                :onchange value-state})))
+                :onChange value-state})))
 
 ;; ----
 
 (c/defn-item add-text [on-submit]
-  (with-bind
+  (c/with-bind
     (fn [bind]
-      (c/with-state-as [st text :local ""]
-        (c/focus lens/second
-                 (dom/form {:onsubmit (merge-handlers (fn [text e]
-                                                        (.preventDefault e)
-                                                        (c/return :state ""))
-                                                      (bind on-submit (f/constantly text)))}
-                           textbox
-                           (dom/button {:type "submit"
-                                        :disabled (empty? text)}
-                                       "Add")))))))
+      (let [on-submit (bind on-submit)]
+        (c/with-state-as [st text :local ""]
+          (c/focus lens/second
+                   (dom/form {:onSubmit (fn [text e]
+                                          (.preventDefault e)
+                                          (c/call on-submit text))}
+                             textbox
+                             (dom/button {:type "submit"
+                                          :disabled (empty? text)}
+                                         "Add"))))))))
 ;; ----
 
 (defrecord Todo [id text done?])
@@ -90,19 +45,21 @@
 (c/defn-item item [on-delete]
   (c/with-state-as todo
     (dom/div (c/focus :done? checkbox)
-             (dom/button {:onclick on-delete} "Zap")
+             (dom/button {:onClick on-delete} "Zap")
              " " (:text todo))))
 
 (c/defn-item item-list [on-delete-item]
   (c/with-state-as todos
-    (with-bind
+    (c/with-bind
       (fn [bind]
-        (apply dom/div
-               (map-indexed (fn [idx id]
-                              (-> (c/focus (lens/at-index idx)
-                                           (item (bind on-delete-item (f/constantly id))))
-                                  (c/keyed id)))
-                            (map :id todos)))))))
+        (let [on-delete-item (bind on-delete-item)]
+          (apply dom/div
+                 (map-indexed (fn [idx id]
+                                (-> (c/focus (lens/at-index idx)
+                                             (item (fn [_]
+                                                     (c/call on-delete-item id))))
+                                    (c/keyed id)))
+                              (map :id todos))))))))
 
 ;; ----
 
@@ -121,12 +78,12 @@
       (update :next-id inc)))
 
 (c/def-item main
-  (with-bind (fn [bind]
-               (c/fragment (dom/h3 "TODO")
-                           (c/focus :todos (item-list (bind delete-item)))
+  (c/with-bind (fn [bind]
+                 (c/fragment (dom/h3 "TODO")
+                             (c/focus :todos (item-list (bind delete-item)))
               
-                           (dom/br)
-                           (add-text (bind add-item))))))
+                             (dom/br)
+                             (add-text (bind add-item))))))
 
 (main/run (.getElementById js/document "content")
   main
