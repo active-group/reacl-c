fc09966b0529be50d07354f502caae17f9c829ff
David Frese <david.frese@active-group.de>
2022-02-07 17:33:30 +0100

    Add a docstring to 'provided'.

--- src/reacl_c/test_util/core.clj
+++ src/reacl_c/test_util/core.clj
@@ -1,5 +1,17 @@
 (ns reacl-c.test-util.core
   (:require [reacl2.test-util.beta :as r-tu]))
 
-(defmacro provided [bindings & body]
+(defmacro provided
+  "Replaces the values bound to the given vars during the
+  evaluation of `body`, and sets them back to the previous values
+  afterwards. Example:
+
+  ```
+  (def x 42)
+  (provided [x 11]
+    (is (= (* x 2) 22)))
+  (is (= x 42))
+  ```
+  "
+  [bindings & body]
   `(r-tu/provided ~bindings ~@body))
