c30f690de60bb42e18d5b9e62a4392ab70832ae4
David Frese <david.frese@active-group.de>
2023-03-27 17:17:40 +0200

    Next dev cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.active-group/reacl-c "0.11.5"
+(defproject de.active-group/reacl-c "0.11.6-SNAPSHOT"
   :description "Compositional and declarative user interface library for Clojure and Clojurescript."
   :url "http://github.com/active-group/reacl-c"
   
