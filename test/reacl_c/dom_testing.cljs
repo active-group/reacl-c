53a814ea23845f70a379b6cbc54ab28c187962ef
David Frese <david.frese@active-group.de>
2024-06-26 19:06:23 +0200

    Test without react test-utils.

--- test/reacl_c/dom_testing.cljs
+++ test/reacl_c/dom_testing.cljs
@@ -1,7 +1,8 @@
 (ns reacl-c.dom-testing
   "Reduced and simplified version of reacl-c.test-util.dom-testing"
   (:require [reacl-c.main :as main]
-            ["react-dom/test-utils" :as react-tu]
+            #_["react-dom/test-utils" :as react-tu]
+            #_["@testing-library/react" :as react-tu]
             [cljs-async.core :as async :include-macros true]))
 
 (defrecord ^:private Env [app node])
@@ -10,13 +11,19 @@
   (let [f (last args)
         options (apply hash-map (drop-last args))]
     (let [e (or (:container options) (js/document.createElement "div"))
-          app (main/run e item {:initial-state (:state options)
-                                :handle-action! (:handle-action! options)})]
-      #_(react-dom/flushSync (fn [] nil)) ;; react > 17
-      (async/try-finally (fn []
-                           (f (Env. app e)))
-                         (fn []
-                           (main/stop! app))))))
+          new-container? (not (:container options))]
+      (when new-container?
+        ;; Note: especially native dispatchEvents don't work if not mounted to the document. (react-tu does work around that)
+        (.appendChild js/document.body e))
+      (let [app (main/run e item {:initial-state (:state options)
+                                  :handle-action! (:handle-action! options)})]
+        #_(react-dom/flushSync (fn [] nil)) ;; react > 17
+        (async/try-finally (fn []
+                             (f (Env. app e)))
+                           (fn []
+                             (main/stop! app)
+                             (when new-container?
+                               (.removeChild js/document.body e))))))))
 
 (defn container [env]
   (:node env))
@@ -32,6 +39,33 @@
 (defn fire-event
   [node event & [event-properties]]
   (assert (= :click event) "only clicks for now")
+  (let [default-props {:bubbles true
+                       :cancelable false}]
+    (.dispatchEvent node (new js/PointerEvent "click" (clj->js (merge default-props event-properties))))))
+
+#_(defn fire-event [node event & [event-properties]]
+  (assert (= :click event) "only clicks for now")
   (if event-properties
     (react-tu/Simulate.click node (js/Event. "click" (clj->js event-properties)))
     (react-tu/Simulate.click node)))
+
+#_(defn- fire-event* [node event]
+  (react-tu/fireEvent node event))
+
+#_(defn fire-event
+  "Fire an event on the given DOM `node`, where `event` can be a DOM
+  Event object, or a keywork like `:click` for standard events. See
+  https://github.com/testing-library/dom-testing-library/blob/master/src/event-map.js
+  for a list."
+  [node event & [event-properties]]
+  (fire-event* node
+               (if (instance? js/Event event)
+                 (do (assert (nil? event-properties) "Additional event properties can not be set on an event object argument.")
+                     event)
+                 ;; Note: createElemnt.click() uses default properties, but createEvent("click") doesn't :-/ damn!
+                 ;; How can we distinguish it? Let's use: keyword event => default; string event => generic.
+                 (let [f (if (string? event)
+                           (partial react-tu/createEvent event)
+                           (or (aget react-tu/createEvent (name event))
+                               (throw (js/Error (str "Not a known standard event: " (pr-str event) ".")))))]
+                   (f node (clj->js event-properties))))))
--- test/reacl_c/main_react_test.cljs
+++ test/reacl_c/main_react_test.cljs
@@ -1,5 +1,5 @@
 (ns reacl-c.main-react-test
-  "Test interop with Reacl"
+  "Test interop with React"
   (:require [reacl-c.main.react :as main]
             [reacl-c.core :as c]
             [reacl-c.dom :as dom]
@@ -7,57 +7,71 @@
             [reacl-c.dom-testing :as dt]
             [cljs.test :refer (is deftest testing async) :include-macros true]))
 
+
+(defn react-rendering [react-elem f]
+  (let [host (js/document.createElement "div")]
+    ;; especially for event, adding it to the document can be necessary.
+    (.appendChild js/document.body host)
+    (r0/render-component react-elem host)
+    (try
+      (f host)
+      (finally
+        (.removeChild js/document.body host)))))
+
 (deftest embed-test
-  (let [e (main/embed (dom/div "Hello World"))
-        host (js/document.createElement "div")]
-    (r0/render-component e host)
-    (let [n (first (array-seq (.-childNodes host)))]
-      (is (= (.-nodeName n) "DIV"))
-      (is (= "Hello World" (.-textContent n))))))
+  (react-rendering
+   (main/embed (dom/div "Hello World"))
+   (fn [host]
+     (let [n (first (array-seq (.-childNodes host)))]
+       (is (= (.-nodeName n) "DIV"))
+       (is (= "Hello World" (.-textContent n)))))))
 
 (deftest send-message-test
   (let [e (main/embed (c/isolate-state "foo"
                                        (c/handle-message (fn [st msg]
                                                            msg)
                                                          (c/dynamic (fn [st]
-                                                                      (dom/div st))))))
-        host (js/document.createElement "div")]
-    (r0/render-component e host)
-    (main/send-message! e "bar")
-    (let [n (first (array-seq (.-childNodes host)))]
-      (is (= (.-nodeName n) "DIV"))
-      (is (= "bar" (.-textContent n))))))
+                                                                      (dom/div st))))))]
+    (react-rendering
+     e
+     (fn [host]
+       (main/send-message! e "bar")
+       (let [n (first (array-seq (.-childNodes host)))]
+         (is (= (.-nodeName n) "DIV"))
+         (is (= "bar" (.-textContent n))))))))
 
 (deftest embed-state-test
   (testing "works basically"
-    (let [state (atom [])
-          e (main/embed (dom/button {:onClick (fn [state ev]
-                                                (c/return :state (cons :click state)))}
-                                    "Hello World")
-                        {:state @state
-                         :set-state! (fn [st]
-                                       (reset! state st))})
-          host (js/document.createElement "div")]
-      (r0/render-component e host)
-      (let [btn (first (array-seq (.-childNodes host)))]
-        (dt/fire-event btn :click)
-        (is (= [:click] @state)))))
+    (let [state (atom [])]
+      (react-rendering
+       (main/embed (dom/button {:onClick (fn [state ev]
+                                           (c/return :state (cons :click state)))}
+                               "Hello World")
+                   {:state @state
+                    :set-state! (fn [st]
+                                  (reset! state st))})
+       (fn [host]
+         (let [btn (first (array-seq (.-childNodes host)))]
+           (assert (some? btn))
+           (dt/fire-event btn :click)
+           (is (= [:click] @state)))))))
 
   (testing "action handlers see simultanous state changes"
     ;; even is :set-state! would do something weird; we must assume
     ;; that a new state takes effect in action handlers; that's a basic guarantee of reacl-c.
-    (let [state (atom [])
-          e (main/embed (-> (dom/button {:onClick (fn [state ev]
-                                                    (c/return :state (conj state :click)
-                                                              :action :foo))}
-                                        "Hello World")
-                            (c/handle-action (fn [state a]
-                                               (c/return :state (conj state :action)))))
-                        {:state @state
-                         :set-state! (fn [st]
-                                       (reset! state st))})
-          host (js/document.createElement "div")]
-      (r0/render-component e host)
-      (let [btn (first (array-seq (.-childNodes host)))]
-        (dt/fire-event btn :click)
-        (is (= [:click :action] @state))))))
+    (let [state (atom [])]
+      (react-rendering
+       (main/embed (-> (dom/button {:onClick (fn [state ev]
+                                               (c/return :state (conj state :click)
+                                                         :action :foo))}
+                                   "Hello World")
+                       (c/handle-action (fn [state a]
+                                          (c/return :state (conj state :action)))))
+                   {:state @state
+                    :set-state! (fn [st]
+                                  (reset! state st))})
+       (fn [host]
+         (let [btn (first (array-seq (.-childNodes host)))]
+           (assert (some? btn))
+           (dt/fire-event btn :click)
+           (is (= [:click :action] @state))))))))
